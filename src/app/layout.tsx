a5da7beb42434cc6fe46d00ee738cb61e721d881
stefan <veteransmut519@gmail.com>
2024-09-19 11:24:08 +1000

    new

---
+++ src/app/layout.tsx
@@ -0,0 +1,61 @@
+import { Metadata } from 'next';
+import * as React from 'react';
+
+import Button from '@/components/buttons/Button';
+
+import '@/styles/globals.css';
+// !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
+import '@/styles/colors.css';
+
+import { siteConfig } from '@/constant/config';
+
+// !STARTERCONF Change these default meta
+// !STARTERCONF Look at @/constant/config to change them
+export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
+  title: {
+    default: siteConfig.title,
+    template: `%s | ${siteConfig.title}`,
+  },
+  description: siteConfig.description,
+  robots: { index: true, follow: true },
+  // !STARTERCONF this is the default favicon, you can generate your own from https://realfavicongenerator.net/
+  // ! copy to /favicon folder
+  icons: {
+    icon: '/favicon/favicon.ico',
+    shortcut: '/favicon/favicon-16x16.png',
+    apple: '/favicon/apple-touch-icon.png',
+  },
+  manifest: `/favicon/site.webmanifest`,
+
+  // authors: [
+  //   {
+  //     name: 'Theodorus Clarence',
+  //     url: 'https://theodorusclarence.com',
+  //   },
+  // ],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html>
+      <body>
+        <div className='flex px-4 justify-between'>
+          <div className=' '>
+            <p>every1.click</p>
+            <p>Secure Your Pixels, Own Your Space</p>
+          </div>
+
+          <div>
+            <p>every1.click</p>
+            <p>Secure Your Pixels, Own Your Space</p>
+          </div>
+        </div>
+        {children}</body>
+    </html>
+  );
+}
