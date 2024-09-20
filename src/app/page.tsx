9a2ace15d93f0a6b030d5d2b328f2fa158e85071
stefan <veteransmut519@gmail.com>
2024-09-20 18:30:37 +1000

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,17 @@ export default function HomePage() {
     <div className='flex flex-col w-full items-center justify-center'>
       <div className="relative sm:p-8 p-2">
         <img src="https://www.collectivemeasures.com/wp-content/uploads/2020/07/fullsizeoutput_1b6-scaled.jpeg" alt="Your Image" className="rounded-lg" />
-        <div className="absolute inset-0 flex flex-col gap-8 items-center justify-center">
-          <p className="flex text-white text-center lg:text-[4vw]  md:text-3xl  text-2xl font-bold px-2">Available Now on Desktop</p>
-          <p className="text-white  lg:text-3xl md:text-2xl text-lg font-bold bg-gray-100 hover:bg-green-400 bg-opacity-50 px-4 py-2 rounded">
+        <div className="absolute lg:top-80 top-48 inset-0 flex flex-col gap-8 items-center">
+          <p className="flex  text-white text-center lg:text-[6vw] md:text-3xl  text-2xl font-bold px-2">Available Now on Desktop</p>
+          <div className="text-white xl:mt-28 lg:text-3xl md:text-2xl text-lg font-bold bg-gray-100 hover:bg-green-400 bg-opacity-50 px-4 py-2 rounded">
             <a href="https://s3.us-east-2.amazonaws.com/every1.click/every1.click+1.1.0.appx" id="overlayDownloadLink" target="_blank">
               Download Now
             </a>
-          </p>
+          </div>
         </div>
       </div>
 
-      <div className='flex flex-col gap-4 justify-center items-center xl:w-[72rem] w-full rounded-lg bg-white border px-2 py-6 mt-8 '>
+      <div className='flex flex-col gap-4 justify-center items-center xl:w-[80rem] w-full rounded-lg bg-white border px-2 py-6 mt-8 '>
         <p className='text-2xl text-center'>Desktop Demo - Learn how to use a corner of your desktop to make money with our toolbar.</p>
         <video className='rounded-md' width="640" height="360" controls muted preload="none">
           <source src="/path/to/video.mp4" type="video/mp4" />
@@ -34,7 +34,7 @@ export default function HomePage() {
         </video>
       </div>
 
-      <div className='flex flex-col gap-4 justify-center items-center xl:w-[72rem] w-full rounded-lg bg-white border p-2 mt-8 '>
+      <div className='flex flex-col gap-4 justify-center items-center xl:w-[80rem] w-full rounded-lg bg-white border p-2 mt-8 '>
         <div className='flex flex-col text-center gap-3 py-4'>
           <p className='text-2xl text-blue-600 font-bold'>Your Data, Your Rules</p>
           <p className='text-lg'> OCYRUS empowers you to take control of your online experience. Stop being the product—start owning your space.</p>
