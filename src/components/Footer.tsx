63367b5b13210fe1f80ea5cf33b2e2becfe06d4b
stefan <veteransmut519@gmail.com>
2024-09-26 21:50:19 +1000

    Modifyed999
    Co-authored-by: versatility519 <veteransmut519@gmail.com>
    Co-authored-by: BinaryBard-0604 <oleksii.kasian.midas@gamil.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,12 +22,15 @@ const Footer: React.FC = () => {
       <div className="flex flex-col md:flex-row md:justify-center lg:gap-8 md:gap-4 px-8 py-8 lg:text-2xl md:text-xl text-base">
         <div>
           <p className=" font-bold py-2">Customer Care</p>
+
           <p>About Us</p>
           <p>Contact Us</p>
           <p>Privacy Policy</p>
         </div>
+
         <div>
           <p className=" font-bold py-2">More</p>
+
           <p>Join</p>
           <p>Download</p>
           <p>Online</p>
